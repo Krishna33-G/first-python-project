245f0a32db8f26d4c4dfec10fa65f88525b9d04e
Krishna -3 <kollisaikrishna5@gmail.com>
2025-06-20 20:56:50 +0530

    Add files via upload
    project for gayamma

---
+++ project2.1.py
@@ -0,0 +1,12 @@
+print("im krishna ask for anything gayathri")
+while True:
+    gayi_ask=input("you:").lower()
+    if "name"in gayi_ask:
+        print("i am your frienduuuu")
+    elif "age"in gayi_ask:
+        print("iam december 19th,2002 born ra gayamma")
+    elif "bye"in gayi_ask:
+        print("bye ra gayamma")
+        break
+else:
+    print(" sorry gayamma idont understand")
